43b8c83f096957a3a6b45bc4b9233ed1cd33031d
Kin Lu <29929622+klu684@users.noreply.github.com>
2019-05-13 10:49:31 -0400

    Updated Utils Folder

---
+++ app/src/main/java/com/example/kin/menupicture/Utils/FilePaths.java
@@ -0,0 +1,14 @@
+package com.example.kin.menupicture.Utils;
+
+import android.os.Environment;
+
+public class FilePaths {
+
+    //"storage/emulated/0"
+    public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
+
+    public String PICTURES = ROOT_DIR + "/Pictures";
+    public String CAMERA = ROOT_DIR + "/DCIM/Camera";
+    public String DOWNLOADS = ROOT_DIR + "/Download";
+
+}
